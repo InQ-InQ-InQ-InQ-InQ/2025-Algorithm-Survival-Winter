545c67210079e489e88787605fa7de336d7de726
jae woo Jung <awfjol2008@gmail.com>
2025-01-12 23:30:57 +0900

    feat: 2주차 문제 1 풀이 완료

--- 정재우/week2/Problem_11725_Silver2.java
+++ 정재우/week2/Problem_11725_Silver2.java
@@ -1,37 +1,46 @@
 package week2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Problem_11725_Silver2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] parent = new int[n + 1];
 
-        List<Integer>[] tree = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) {
+        List<Integer>[] tree = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
-
-        for (int i = 1; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        boolean[] visited = new boolean[n + 1];
+        StringTokenizer st;
+        for (int i = 1; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
             tree[a].add(b);
             tree[b].add(a);
         }
 
-        for (int i = 2; i <= N; i++) {
-            System.out.print(i + ": ");
-            for (int j = 0; j < tree[i].size(); j++) {
-                System.out.print(tree[i].get(j) + " ");
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : tree[cur]) {
+                if (visited[next]) {
+                    continue;
+                }
+                visited[next] = true;
+                queue.add(next);
+                parent[next] = cur;
             }
-            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(parent[i]);
         }
     }
 }
