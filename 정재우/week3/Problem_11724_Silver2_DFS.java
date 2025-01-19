ea056bcb214e97366201d97ee9b35997a9324b69
jae woo Jung <awfjol2008@gmail.com>
2025-01-19 23:57:44 +0900

    feat: 3주차 문제 2 풀이 완료

---
+++ 정재우/week3/Problem_11724_Silver2_DFS.java
@@ -0,0 +1,60 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Problem_11724_Silver2_DFS {
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= m; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st1.nextToken());
+            int v = Integer.parseInt(st1.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void dfs(int start) {
+        if (visited[start]) {
+            return;
+        }
+        visited[start] = true;
+        for (int i : graph[start]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
