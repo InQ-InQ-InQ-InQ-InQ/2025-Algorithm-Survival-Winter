4bfa4a729af67ec88af537363396361db54e3657
jae woo Jung <awfjol2008@gmail.com>
2025-01-23 18:32:56 +0900

    feat: 4주차 문제 1 난이도 표기 수정

--- 정재우/week4/Problem_1012_Silver2_DFS.java
+++ 정재우/week4/Problem_1012_Silver2_DFS.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Problem_1012_Silver_DFS {
+public class Problem_1012_Silver2_DFS {
     static boolean[][] map;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
