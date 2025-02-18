c1d61a67eb7a098ba1ad7f40048db88b6f7cb3be
chyun7114 <chyun7114@naver.com>
2025-02-18 23:00:49 +0900

    feat : [Silver I] 점프왕 쩰리 (Large) - 16174 upload

--- 윤창현/7주차/[Gold III] 벽 부수고 이동하기 - 2206/Main.java
+++ 윤창현/7주차/[Gold III] 벽 부수고 이동하기 - 2206/Main.java
@@ -13,8 +13,9 @@ public class Main {
     static int result = Integer.MIN_VALUE;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+
     // 위치 변수
-    static class Loc{
+    static class Loc {
         int x;
         int y;
         int count;
@@ -39,9 +40,9 @@ public class Main {
         map = new int[n][m];
         visited = new boolean[n][m][2];     // 벽을 부순 경우 1, 부수지 않은 경우 0
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String temp = br.readLine();
-            for(int j = 0; j < m; j++){
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(String.valueOf(temp.charAt(j)));
             }
         }
@@ -49,7 +50,7 @@ public class Main {
         Queue<Loc> queue = new LinkedList<>();
 
         queue.add(new Loc(0, 0, 1, false));
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Loc nowLoc = queue.poll();
 
             // 도착점에 도달한 경우
@@ -63,25 +64,25 @@ public class Main {
             int nowCount = nowLoc.count;
             boolean nowIsDestroy = nowLoc.isDestroy;
 
-            for(int i = 0; i < dx.length; i++){
+            for (int i = 0; i < dx.length; i++) {
                 int nx = nowX + dx[i];
                 int ny = nowY + dy[i];
 
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
                 // 먼저 다음 갈 곳을 생각하면서 문제를 해결한다.
                 // 다음 갈 곳이 벽이 아니라면?
-                if(map[nx][ny] == 0){
+                if (map[nx][ny] == 0) {
                     // 아직까지 벽을 부수지 않았다면
-                    if(!visited[nx][ny][0] && !nowIsDestroy){
+                    if (!visited[nx][ny][0] && !nowIsDestroy) {
                         visited[nx][ny][0] = true;
                         queue.add(new Loc(nx, ny, nowCount + 1, false));
-                    }else if(!visited[nx][ny][1] && nowIsDestroy){
+                    } else if (!visited[nx][ny][1] && nowIsDestroy) {
                         visited[nx][ny][1] = true;
                         queue.add(new Loc(nx, ny, nowCount + 1, true));
                     }
-                } else if(map[nx][ny] == 1) {
-                    if(!visited[nx][ny][1] && !nowIsDestroy){
+                } else if (map[nx][ny] == 1) {
+                    if (!visited[nx][ny][1] && !nowIsDestroy) {
                         visited[nx][ny][1] = true;
                         queue.add(new Loc(nx, ny, nowCount + 1, true));
                     }
